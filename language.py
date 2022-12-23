83eb0ea5f09438faaf6c7cd5df5a2439d018220b
Anushaanu1510 <119996233+Anushaanu1510@users.noreply.github.com>
2022-12-23 15:46:19 +0530

    Add files via upload

---
+++ language.py
@@ -0,0 +1,12 @@
+print("Enter 1 for English")
+print("Enter 2 for Hindi")
+print("Enter 3 for Kannada")
+choice=int(input("Enter your choice:"))
+if choice==1:
+    print("You have selected the language English")
+elif choice==2:
+    print("You have selected the language Hindi")
+elif choice==3:
+    print("You have selected the language Kannada")
+else:
+    print("Wrong choice")
