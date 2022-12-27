ccd33431a49a67d3d4f78fa3c90fec88d32e85fa
Anushaanu1510 <119996233+Anushaanu1510@users.noreply.github.com>
2022-12-27 15:07:39 +0530

    Delete hoyrstominute.py

--- hoyrstominute.py
+++
@@ -1,8 +0,0 @@
-def hours_to_minute(hours):
-    minutes=hours*60
-    seconds=hours*3600
-    print("there are",minutes, "minutes in", hours,"hours")
-    print("there are",seconds, "seconds in", hours,"hours")
-
-hours=int(input("Enter the hour"))
-hours_to_minute(hours)
