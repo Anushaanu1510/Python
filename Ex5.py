172a826dfe8046ba169eb074a65b050281e98650
Anushaanu1510 <119996233+Anushaanu1510@users.noreply.github.com>
2022-12-27 15:14:11 +0530

    Add files via upload

---
+++ Ex5.py
@@ -0,0 +1,14 @@
+#x = int(input("Enter the value for x"))
+#y = int(input("Enter the value for y"))
+#z = x + y
+#print(z)
+
+a = "hello how are you"
+print(a)
+
+greet = [1,100,"hi", "welcome", "hola", "hi", "bonjour", "salve", "welcome", "hello", "howdy", "hola"]
+print(greet)
+greet = ["hi", "welcome", "hola", "hi", "bonjour", "salve", "welcome", "hello", "howdy", "hola"]
+print(set(greet))
+#duplicates are not allowed
+print(greet.count("hi"))
